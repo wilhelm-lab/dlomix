7bcc67e7e660083b71e160517c070b92792ef620
DESKTOP-NJTIKKQ\OS <o.shouman@tum.de>
2021-06-01 10:40:38 +0200

    example test for pipeline and deeplc

---
+++ test_deeplc.py
@@ -0,0 +1,27 @@
+from eval_utils import delta99_metric, delta95_metric
+from deeplc import DeepLC
+from data import RTDataset
+import pickle
+import tensorflow as tf
+
+model = DeepLC(seq_length=50)
+opt = tf.keras.optimizers.SGD(learning_rate=1e-4, momentum=0.9)
+
+model.compile(optimizer=opt,
+              loss='mse',
+              metrics=['mean_absolute_error', delta95_metric, delta99_metric])
+
+
+DATAPATH = '/scratch/RT_raw/iRT_ProteomeTools_ReferenceSet.csv'
+
+d = RTDataset(data_source=DATAPATH, pad_length=50, batch_size=512, val_ratio=0.2,
+              path_aminoacid_atomcounts="./lookups/aa_comp_rel.csv")
+
+history = model.fit(d.tf_dataset['train'], epochs=3, validation_data=d.tf_dataset['val'])
+
+model.save_weights('./API-Overview/deeplc_test_sgd')
+
+with open("./history_deeplc_sgd.pkl", 'wb') as f:
+   pickle.dump(history.history, f)
+
+
---
+++ test_pipeline.py
@@ -0,0 +1,45 @@
+from data import RTDataset
+from eval_utils import delta95_metric
+from models import *
+
+# data path
+DATAPATH = '/scratch/RT_raw/iRT_ProteomeTools_ReferenceSet.csv'
+
+'''
+create dataset:
+    - abstracts TF Dataset
+    - splits sequences
+    - padds sequences to the specified length
+    - performs train/val split if needed
+'''
+d = RTDataset(data_source=DATAPATH, sequence_col="sequence", target_col="irt",
+              pad_length=40, normalize_targets=True,
+              batch_size=128, val_ratio=0.2)
+
+
+'''
+create Model:
+    - abstracts a RT Prediction Model
+    - encoding to integers happens at beginning of the model
+    - model has three main parts (embedding, encoder, regressor) with some parametrization
+    '''
+
+model = RTPredictor(embeddings_count=len(ALPHABET_UNMOD), embedding_dim=300, seq_length=40, encoder='lstm')
+
+
+''''
+
+Target is to always have the high-level model functions callabe to abstract out training, but still people
+can write their own custom training loops since the model extends tf.keras.Model
+
+ '''
+
+model.compile(optimizer='adam',
+              loss='mse',
+              metrics=['mean_absolute_error', delta95_metric])
+
+history = model.fit(d.get_tf_dataset('train'), epochs=15, validation_data=d.get_tf_dataset('val'))
+
+'''
+
+'''
