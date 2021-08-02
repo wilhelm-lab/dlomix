eab84227676eeacf19c6911577b34e6150f2e0b1
Omar Shouman <o.shouman@tum.de>
2021-08-02 14:03:28 +0200

    added prosit [WIP]

---
+++ dlpro/models/prosit.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow.keras.layers.experimental import preprocessing
+from dlpro.constants import ALPHABET_UNMOD
+
+
+class PrositRetentionTimePredictor(tf.keras.Model):
+    def __init__(self, embedding_input_dim=23, embedding_output_dim=16, seq_length=30, vocab_dict=ALPHABET_UNMOD,
+                 encoder_layer_type="gru"):
+        super(PrositRetentionTimePredictor, self).__init__()
+
+        self.string_lookup = preprocessing.StringLookup(vocabulary=list(vocab_dict.keys()))
+
+        self.embedding = tf.keras.layers.Embedding(input_dim=embedding_input_dim, output_dim=embedding_output_dim,
+                                                   input_length=seq_length)
+        self._build_encoder()
+
+        self.attention = tf.keras.layers.Attention()
+
+        self.regressor = tf.keras.Sequential([
+            tf.keras.layers.Dense(512, activation='relu'),
+            tf.keras.layers.LeakyReLU(alpha=0.3),
+            tf.keras.layers.Dropout(rate=0.1)
+        ])
+
+        self.output_layer = tf.keras.layers.Dense(1)
+
+    def _build_encoder(self):
+        self.encoder = tf.keras.Sequential([
+            tf.keras.layers.Bidirectional(tf.keras.layers.GRU(units=256, return_sequences=True)),
+            tf.keras.layers.Dropout(rate=0.1),
+            tf.keras.layers.GRU(units=512, return_sequences=True),
+            tf.keras.layers.Dropout(rate=0.1)
+        ])
+
+    def call(self, inputs, **kwargs):
+        x = self.string_lookup(inputs)
+        x = self.embedding(x)
+        x = self.encoder(x)
+        x = self.attention(x)
+        x = self.regressor(x)
+        x = self.output_layer(x)
+        return x
---
+++ test/test_prosit.py
@@ -0,0 +1,30 @@
+from dlpro.eval.rt_eval import delta99_metric, delta95_metric
+from dlpro.models.prosit import PrositRetentionTimePredictor
+from dlpro.data.data import RetentionTimeDataset
+import pickle
+import tensorflow as tf
+
+model = PrositRetentionTimePredictor()
+print(model.summary())
+
+reduceLR = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, min_lr=0)
+
+optimizer = tf.keras.optimizers.Adam(lr=0.0001,decay=1e-7)
+
+model.compile(optimizer=optimizer,
+              loss='mse',
+              metrics=['mean_absolute_error', delta95_metric, delta99_metric])
+
+
+DATAPATH = '/scratch/RT_raw/iRT_ProteomeTools_ReferenceSet.csv'
+
+d = RetentionTimeDataset(data_source=DATAPATH, pad_length=30, batch_size=512, val_ratio=0.2)
+
+history = model.fit(d.tf_dataset['train'], epochs=2, validation_data=d.tf_dataset['val'], callbacks=[reduceLR])
+
+model.save_weights('./API-Overview/prosit_test_adam')
+
+with open("./history_prosit_test_adam.pkl", 'wb') as f:
+   pickle.dump(history.history, f)
+
+
