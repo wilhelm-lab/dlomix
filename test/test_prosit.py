07a1097f6960d6d5123ae8b502039179f3c84179
Omar Shouman <o.shouman@tum.de>
2021-08-03 19:18:15 +0200

    clean tests

--- test/test_prosit.py
+++ test/test_prosit.py
@@ -1,21 +1,24 @@
+import os
+import sys
+import pickle
+import tensorflow as tf
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from dlpro.eval.rt_eval import delta99_metric, delta95_metric
 from dlpro.models.prosit import PrositRetentionTimePredictor
 from dlpro.data.data import RetentionTimeDataset
-import pickle
-import tensorflow as tf
 
 
 model = PrositRetentionTimePredictor()
 
 reduceLR = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, min_lr=0)
 
-optimizer = tf.keras.optimizers.Adam(lr=0.0001,decay=1e-7)
+optimizer = tf.keras.optimizers.Adam(lr=0.0001, decay=1e-7)
 
 model.compile(optimizer=optimizer,
               loss='mse',
               metrics=['mean_absolute_error', delta95_metric, delta99_metric])
-
 
 DATAPATH = '/scratch/RT_raw/iRT_ProteomeTools_ReferenceSet.csv'
 
@@ -26,6 +29,4 @@ history = model.fit(d.tf_dataset['train'], epochs=2, validation_data=d.tf_datase
 model.save_weights('./API-Overview/prosit_test_adam')
 
 with open("./history_prosit_test_adam.pkl", 'wb') as f:
-   pickle.dump(history.history, f)
-
-
+    pickle.dump(history.history, f)
