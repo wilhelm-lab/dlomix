91a3e45ff8416db88a3cf029d5c52e799391e047
Omar Shouman <o.shouman@tum.de>
2021-08-23 17:11:48 +0200

    timedelta metric refactoring

--- dlpro/eval/rt_eval.py
+++ dlpro/eval/rt_eval.py
@@ -21,21 +21,32 @@ def delta99_metric(y_true, y_pred):
 
 
 class TimeDeltaMetric(tf.keras.metrics.Metric):
-    def __init__(self, mean=0, std=1, percentage=0.95, name='timedelta_metric', **kwargs):
+    def __init__(self, mean=0, std=1, percentage=0.95, name='timedelta_metric', rescale_targets=True, **kwargs):
         super(TimeDeltaMetric, self).__init__(name=name, **kwargs)
         self.delta = self.add_weight(name='delta', initializer='zeros')
         self.mean = mean
         self.std = std
         self.percentage = percentage
+        self.rescale_targets = rescale_targets
 
     def update_state(self, y_true, y_pred, sample_weight=None):
-        y_true = y_true * self.std + self.mean
+
+        # rescale
+        if self.rescale_targets:
+            y_true = y_true * self.std + self.mean
+
         y_pred = y_pred * self.std + self.mean
 
-        mark = tf.cast(
-            tf.cast(tf.shape(y_true)[0], dtype=tf.float32) * self.percentage, dtype=tf.int32)
+        # find position of the index
+        length = tf.shape(y_true)[0]
+        mark = tf.cast(length, dtype=tf.float32) * self.percentage
+        mark = tf.cast(mark, dtype=tf.int32)
+
+        # compute residuals and sort
         abs_error = tf.abs(y_true - y_pred)
         delta = tf.sort(abs_error)[mark - 1]
+
+        # two-sided delta
         self.delta.assign(tf.reduce_sum(delta * 2))
 
     def result(self):
