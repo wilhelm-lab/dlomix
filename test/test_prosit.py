c8f1b8f05291361ec7204b8a2cf3d183f640bba4
Omar Shouman <o.shouman@tum.de>
2021-08-25 14:52:58 +0200

    prosit test script

--- test/test_prosit.py
+++ test/test_prosit.py
@@ -2,36 +2,59 @@ import os
 import sys
 import pickle
 import tensorflow as tf
+import pandas as pd
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-from dlpro.eval.rt_eval import delta99_metric, delta95_metric, TimeDeltaMetric
+from dlpro.eval.rt_eval import TimeDeltaMetric
 from dlpro.models.prosit import PrositRetentionTimePredictor
 from dlpro.data.RetentionTimeDataset import RetentionTimeDataset
-
+from dlpro.reports.RetentionTimeReport import RetentionTimeReport
 
 # TODO: start from a saved model
 
-model = PrositRetentionTimePredictor()
+model = PrositRetentionTimePredictor(seq_length=30)
 
-reduceLR = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, min_lr=0)
+#reduceLR = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, min_lr=0)
 
 optimizer = tf.keras.optimizers.Adam(lr=0.0001, decay=1e-7)
 
-DATAPATH = '/scratch/RT_raw/iRT_ProteomeTools_ReferenceSet.csv'
+TRAIN_DATAPATH = '/scratch/TMT/tmt_train_data.csv'
+TEST_DATAPATH = '/scratch/TMT/tmt_test_data.csv'
+#DATAPATH = '/scratch/RT_raw/iRT_ProteomeTools_ReferenceSet.csv'
 
-d = RetentionTimeDataset(data_source=DATAPATH, pad_length=30, batch_size=512, val_ratio=0.2)
+d = RetentionTimeDataset(data_source=TRAIN_DATAPATH, pad_length=30, batch_size=512, val_ratio=0.2)
 
 model.compile(optimizer=optimizer,
               loss='mse',
-              metrics=['mean_absolute_error', delta95_metric, TimeDeltaMetric(d.data_mean, d.data_std)])
+              metrics=['mean_absolute_error', TimeDeltaMetric(d.data_mean, d.data_std)])
+
+weights_file = "./prosit_tmt_fullrun"
+checkpoint = tf.keras.callbacks.ModelCheckpoint(weights_file, save_best_only=True, save_weights_only=True)
+early_stop = tf.keras.callbacks.EarlyStopping(patience=10)
+decay = tf.keras.callbacks.ReduceLROnPlateau(patience=2, factor=0.2)
+callbacks = [checkpoint, early_stop, decay]
 
 
-history = model.fit(d.tf_dataset['train'], epochs=2, validation_data=d.tf_dataset['val'], callbacks=[reduceLR])
+history = model.fit(d.train_data, epochs=100, validation_data=d.val_data, callbacks=callbacks)
 
-model.save_weights('./API-Overview/prosit_test_adam')
+test_rtdata = RetentionTimeDataset(data_source=TEST_DATAPATH,
+                              pad_length=30, batch_size=256, test=True)
+
+predictions = model.predict(test_rtdata.test_data)
+predictions = d.denormalize_targets(predictions)
+test_targets = test_rtdata.get_split_targets(split="test")
+
+report = RetentionTimeReport(output_path="./output", history=history)
+
+print(report.calculate_r2(test_targets, predictions))
+
+pd.DataFrame(
+    {"sequence": test_rtdata.sequences, "irt": test_rtdata.targets, "predicted_irt": predictions}
+).to_csv("./predictions_prosit_fullrun.csv")
+
 
 # TODO: function to store and load history object
-with open("./history_prosit_test_adam.pkl", 'wb') as f:
+with open("./history_prosit_fullrun.pkl", 'wb') as f:
     pickle.dump(history.history, f)
 
