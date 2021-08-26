c8bd3cf52c8403c6c58896b0159a862d7b0ba03f
Omar Shouman <o.shouman@tum.de>
2021-08-26 06:47:06 +0200

    adjust dropout defaults prosit

--- dlpro/models/prosit.py
+++ dlpro/models/prosit.py
@@ -6,13 +6,14 @@ from dlpro.layers.attention import AttentionLayer
 
 class PrositRetentionTimePredictor(tf.keras.Model):
     def __init__(self, embedding_output_dim=16, seq_length=30, vocab_dict=ALPHABET_UNMOD,
-                 dropout_rate=0.1, recurrent_layers_sizes=(256, 512), regressor_layer_size=512):
+                 dropout_rate=0.5, latent_dropout_rate=0.1, recurrent_layers_sizes=(256, 512), regressor_layer_size=512):
         super(PrositRetentionTimePredictor, self).__init__()
 
         # tie the count of embeddings to the size of the vocabulary (count of amino acids)
         self.embeddings_count = len(vocab_dict) + 2
 
         self.dropout_rate = dropout_rate
+        self.latent_dropout_rate = latent_dropout_rate
         self.regressor_layer_size = regressor_layer_size
         self.recurrent_layers_sizes = recurrent_layers_sizes
 
@@ -27,7 +28,7 @@ class PrositRetentionTimePredictor(tf.keras.Model):
 
         self.regressor = tf.keras.Sequential([
             tf.keras.layers.Dense(self.regressor_layer_size, activation='relu'),
-            tf.keras.layers.Dropout(rate=self.dropout_rate)
+            tf.keras.layers.Dropout(rate=self.latent_dropout_rate)
         ])
 
         self.output_layer = tf.keras.layers.Dense(1)
