165460e538c89f642324bf9af6a69773e2114e47
Omar Shouman <o.shouman@tum.de>
2021-09-01 10:56:22 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='dlpro',
+     version='0.1',
+     author="Omar Shouman",
+     author_email="omar.shouman@gmail.com",
+     description="Deep Learning in Proteomics",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/omsh/dlpro",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
