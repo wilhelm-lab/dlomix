8ac643c16ed09e9422fa7854568c5e4d3af739ce
Omar Shouman <o.shouman@tum.de>
2021-09-01 11:44:32 +0200

    add dependencies

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setuptools.setup(
    long_description_content_type="text/markdown",
      url="https://github.com/omsh/dlpro",
      packages=setuptools.find_packages(),
+    install_requires=[
+          'pandas', 'numpy', 'tensorflow'],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
