09d7b833a18d5ffa4d907e08737d19b6e20b779c
Omar Shouman <o.shouman@tum.de>
2021-11-24 09:27:31 +0100

    updated setup.py with extras dev requirements

--- setup.py
+++ setup.py
@@ -7,17 +7,31 @@ setuptools.setup(
     name='dlomix',
     version='0.0.1',
     author="Omar Shouman",
-    author_email="omar.shouman@tum.de",
+    author_email="o.shouman@tum.de",
     description="Deep Learning for Proteomics",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/wilhelm-lab/dlomix",
     packages=setuptools.find_packages(),
     install_requires=[
-        'pandas', 'numpy', 'matplotlib', 'scikit-learn', 'tensorflow'],
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'scikit-learn',
+        'tensorflow'],
+    extras_require={
+        "dev": [
+            "pytest >= 3.7",
+            "twine",
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research"
     ],
 )
