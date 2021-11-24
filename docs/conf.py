dafd2d28e1c1f1cd89d2622a73c18341eaf0a4d6
omsh <omar.shouman@gmail.com>
2021-11-24 10:51:09 +0100

    conf.py updates

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../dlomix/'))
 # -- Project information -----------------------------------------------------
 
 project = 'DLOmix'
-copyright = '2021, Omar Shouman'
+copyright = '2021, Wilhelm Lab'
 author = 'Omar Shouman'
 
 # The full version, including alpha/beta/rc tags
@@ -30,8 +30,11 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = [
+    'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
