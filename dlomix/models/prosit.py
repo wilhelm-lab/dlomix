fc290970d35f65a02f67c73c690e1168f434d2c3
WassimG <wassim.gabriel@gmail.com>
2023-02-25 14:20:52 +0100

    fixed the decoder param for layer size

--- dlomix/models/prosit.py
+++ dlomix/models/prosit.py
@@ -189,7 +189,7 @@ class PrositIntensityPredictor(tf.keras.Model):
         self.decoder = tf.keras.Sequential(
             [
                 tf.keras.layers.GRU(
-                    units=self.recurrent_layers_sizes[1],
+                    units=self.regressor_layer_size,
                     return_sequences=True,
                     name="decoder",
                 ),
