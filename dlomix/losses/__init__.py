83cef7659fd7e26bd8aeb5c8ae93b318be8243a9
Markus Haak <markus.haak@posteo.de>
2023-07-20 17:44:36 +0200

    losses and global seeding

--- dlomix/losses/__init__.py
+++ dlomix/losses/__init__.py
@@ -1,3 +1,5 @@
 from .intensity import masked_spectral_distance, masked_pearson_correlation_distance
+from .quantile import QuantileLoss
+from .conformal import IntervalSize, AbsoluteIntervalSize, RelativeCentralDistance, ConformalScore, ConformalQuantile
 
 __all__ = [masked_spectral_distance, masked_pearson_correlation_distance]
