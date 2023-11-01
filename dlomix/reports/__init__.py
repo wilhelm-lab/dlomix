3432b06122e6f314fc86204eebaad3448c7ad6e6
omsh <omar.shouman@gmail.com>
2023-11-01 19:10:33 +0200

    refactored main RT report wandb file

--- dlomix/reports/__init__.py
+++ dlomix/reports/__init__.py
@@ -1,8 +1,9 @@
 from .IntensityReport import IntensityReport
 from .RetentionTimeReport import RetentionTimeReport
+from .RetentionTimeReportWandb import RetentionTimeReportWandb
 
 __all__ = ["RetentionTimeReport",
            "IntensityReport",
-           "RetentionTimeReport_WANDB",
-           "RetentionTimeReport_CompModels_WANDB"
+           "RetentionTimeReportWandb",
+           "RetentionTimeReport_CompModels_WANDB",
            ]
