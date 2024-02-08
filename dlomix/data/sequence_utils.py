595140e632c8bcf078d05bb589cd4c16c5235db4
omsh <omar.shouman@gmail.com>
2024-02-08 14:12:36 +0200

    modified seq utils not to add empty n-c terms

--- dlomix/data/sequence_utils.py
+++ dlomix/data/sequence_utils.py
@@ -2,8 +2,10 @@ def rebuild_proforma_sequence(seq, mod, n_term, c_term, no_mod_value=-1):
     proforma_sequence = [
         f"{s}[UNIMOD:{m}]" if m != no_mod_value else s for s, m in zip(seq, mod)
     ]
-    proforma_sequence.insert(
-        0, f"[UNIMOD:{n_term}]-" if n_term != no_mod_value else "[]-"
-    )
-    proforma_sequence.append(f"-[UNIMOD:{c_term}]" if c_term != no_mod_value else "-[]")
+
+    if n_term != no_mod_value:
+        proforma_sequence.insert(0, f"[UNIMOD:{n_term}]-")
+
+    if c_term != no_mod_value:
+        proforma_sequence.append(f"-[UNIMOD:{c_term}]")
     return proforma_sequence
