e1fc23deed939c8af6b4e1f37ec55ed1d520e4c4
Julian Poschenrieder <juliposch@aol.com>
2024-05-31 10:37:39 +0000

    dataset processing now working

--- bmpc_shared_scripts/prepare_dataset/prepare_dataset.py
+++ bmpc_shared_scripts/prepare_dataset/prepare_dataset.py
@@ -10,15 +10,15 @@ with open(args.config, 'r') as yaml_file:
     config = yaml.safe_load(yaml_file)
 
 import os
-os.environ['HF_HOME'] = config['paths']['hf_home']
-os.environ['HF_DATASETS_CACHE'] = config['paths']['hf_cache']
+os.environ['HF_HOME'] = config['dataset']['hf_home']
+os.environ['HF_DATASETS_CACHE'] = config['dataset']['hf_cache']
 
 
 # load dataset
 from dlomix.data import FragmentIonIntensityDataset
 from dlomix.constants import PTMS_ALPHABET
 
-datset_base_path = config['paths']['parquet_path']
+datset_base_path = config['dataset']['parquet_path']
 dataset_train_path = f"{datset_base_path}_train.parquet"
 dataset_val_path = f"{datset_base_path}_val.parquet"
 dataset_test_path = f"{datset_base_path}_test.parquet"
@@ -29,14 +29,14 @@ dataset = FragmentIonIntensityDataset(
     test_data_source=dataset_test_path,
     data_format="parquet", 
     # val_ratio=config['dataloader']['val_ratio'], # why do we need this if we already have splits?
-    batch_size=config['dataloader']['batch_size'],
-    max_seq_len=config['dataloader']['seq_length'],
+    batch_size=config['dataset']['batch_size'],
+    max_seq_len=config['dataset']['seq_length'],
     encoding_scheme="naive-mods",
     alphabet=PTMS_ALPHABET,
-    num_proc=config['misc']['num_proc'],
+    num_proc=config['processing']['num_proc'],
     model_features=["precursor_charge_onehot", "collision_energy_aligned_normed","method_nbr"]
 )
 
 
 # save processed dataset to disk
-dataset.save_to_disk(config['paths']['output_path'])
+dataset.save_to_disk(config['dataset']['processed_path'])
