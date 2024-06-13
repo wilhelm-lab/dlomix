ba39c71619c19a7867c83156965a170cd4846923
Julian Poschenrieder <juliposch@aol.com>
2024-06-13 11:29:05 +0200

    improved baseline training, removed num_proc from all configs

--- bmpc_shared_scripts/baseline_training/baseline_model_training.py
+++ bmpc_shared_scripts/baseline_training/baseline_model_training.py
@@ -1,105 +1,29 @@
 import argparse
-import yaml
 import os
-import uuid
+from src.model_training import load_config, model_training, combine_into
 
-import wandb
-from wandb.integration.keras import WandbCallback
-
-from dlomix.constants import PTMS_ALPHABET
-from dlomix.data import load_processed_dataset
-from dlomix.models import PrositIntensityPredictor
-from dlomix.losses import masked_spectral_distance, masked_pearson_correlation_distance
-
-import tensorflow as tf
-from tensorflow.keras.callbacks import EarlyStopping
-
+# parse args
 parser = argparse.ArgumentParser(prog='Baseline Model Training')
 parser.add_argument('--config', type=str, required=True)
-parser.add_argument('--tf-device-nr', type=str, required=True)
+parser.add_argument('--cuda-device-nr', type=str, required=False)
+parser.add_argument('--cpu-threads', type=str, required=False)
+
 args = parser.parse_args()
 
-with open(args.config, 'r') as yaml_file:
-    config = yaml.safe_load(yaml_file)
 
-import os
-os.environ['HF_HOME'] = config['dataset']['hf_home']
-os.environ['HF_DATASETS_CACHE'] = config['dataset']['hf_cache']
+# create config
+config = load_config(args.config)
+if "project" not in config:
+    config["project"] = "baseline model training"
 
-os.environ["CUDA_VISIBLE_DEVICES"] = args.tf_device_nr
+overwritten_params = {
+    "processing": {}
+}
+if args.cuda_device_nr is not None:
+    overwritten_params['processing']['cuda_device_nr'] = args.cuda_device_nr
+if args.cpu_threads is not None:
+    overwritten_params['processing']['num_proc'] = args.cpu_threads
 
-
-config['run_id'] = uuid.uuid4()
-
-project_name = f'baseline model training'
-wandb.init(
-    project=project_name,
-    config=config,
-    tags=[config['dataset']['name']]
-)
-
-# load dataset
-dataset = load_processed_dataset(wandb.config['dataset']['processed_path'])
-
-# initialize relevant stuff for training
-optimizer = tf.keras.optimizers.Adam(learning_rate=wandb.config['training']['learning_rate'])
-
-
-early_stopping = EarlyStopping(
-    monitor="val_loss",
-    min_delta=0.001,
-    patience=20,
-    restore_best_weights=True)
-
-
-if 'load_path' in wandb.config['model']:
-    print(f"loading model from file {wandb.config['model']['load_path']}")
-    model = tf.keras.models.load_model(wandb.config['model']['load_path'])
-else:
-    input_mapping = {
-            "SEQUENCE_KEY": "modified_sequence",
-            "COLLISION_ENERGY_KEY": "collision_energy_aligned_normed",
-            "PRECURSOR_CHARGE_KEY": "precursor_charge_onehot",
-            "FRAGMENTATION_TYPE_KEY": "method_nbr",
-        }
-
-    meta_data_keys=["collision_energy_aligned_normed", "precursor_charge_onehot", "method_nbr"]
-
-    model = PrositIntensityPredictor(
-        seq_length=wandb.config['dataset']['seq_length'],
-        alphabet=PTMS_ALPHABET,
-        use_prosit_ptm_features=False,
-        with_termini=False,
-        input_keys=input_mapping,
-        meta_data_keys=meta_data_keys
-    )
-
-model.compile(
-    optimizer=optimizer,
-    loss=masked_spectral_distance,
-    metrics=[masked_pearson_correlation_distance]
-)
-
-
-# train model
-model.fit(
-    dataset.tensor_train_data,
-    validation_data=dataset.tensor_val_data,
-    epochs=wandb.config['training']['num_epochs'],
-    callbacks=[WandbCallback(save_model=False, log_batch_frequency=True), early_stopping]
-)
-
-out_path = None
-
-if 'save_dir' in wandb.config['model']:
-    out_path = f"{wandb.config['model']['save_dir']}/{wandb.config['dataset']['name']}/{wandb.config['run_id']}.keras"
-
-if 'save_path' in wandb.config['model']:
-    out_path = wandb.config['model']['save_path']
-
-if out_path is not None:
-    model.save(out_path)
-
-
-# finish up training process
-wandb.finish()
+# start run
+combine_into(overwritten_params, config)
+model_training(config)()
