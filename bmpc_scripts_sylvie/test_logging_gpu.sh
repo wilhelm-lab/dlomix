4dd4720dbdd64793fd4dac79398899f6b12e9508
Sylvie Baier <sylvie.baier@tum.de>
2024-08-03 17:30:20 +0200

    Logging in progress

---
+++ bmpc_scripts_sylvie/test_logging_gpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=dlomix_test_logging
+#SBATCH --out=R-%x.%j.out
+#SBATCH --partition=compms-gpu-a40
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+
+# script
+source /nfs/home/students/s.baier/miniconda3/etc/profile.d/conda.sh
+conda activate dlomix
+python /nfs/home/students/s.baier/mapra/dlomix/bmpc_shared_scripts/refinement_transfer_learning/usage_example_automatic_refinement_learning.py
+
+
---
+++ bmpc_shared_scripts/refinement_transfer_learning/usage_example_automatic_refinement_learning.py
@@ -0,0 +1,41 @@
+import os
+
+os.environ['HF_HOME'] = '/cmnfs/proj/bmpc_dlomix/datasets'
+os.environ['HF_DATASETS_CACHE'] = '/cmnfs/proj/bmpc_dlomix/datasets/hf_cache'
+
+num_proc = 16
+os.environ["OMP_NUM_THREADS"] = f"{num_proc}"
+os.environ["TF_NUM_INTRAOP_THREADS"] = f"{num_proc}"
+os.environ["TF_NUM_INTEROP_THREADS"] = f"{num_proc}"
+
+
+import tensorflow as tf
+tf.config.threading.set_inter_op_parallelism_threads(num_proc)
+tf.config.threading.set_intra_op_parallelism_threads(num_proc)
+
+from dlomix.data import load_processed_dataset
+
+dataset = load_processed_dataset('/cmnfs/proj/bmpc_dlomix/datasets/processed/ptm_baseline_small_cleaned_bs1024')
+
+from dlomix.models import PrositIntensityPredictor
+from dlomix.losses import masked_spectral_distance, masked_pearson_correlation_distance
+
+model = tf.keras.models.load_model('/cmnfs/proj/bmpc_dlomix/models/baseline_models/noptm_baseline_full_bs1024_unmod_extended/7ef3360f-2349-46c0-a905-01187d4899e2.keras')
+
+from dlomix.refinement_transfer_learning.automatic_rl_tl import AutomaticRlTlTraining, AutomaticRlTlTrainingConfig
+
+config = AutomaticRlTlTrainingConfig(
+    dataset=dataset,
+    baseline_model=model,
+    min_warmup_sequences_new_weights = 100, 
+    min_warmup_sequences_whole_model = 100, 
+    improve_further = True,
+    use_wandb=False
+)
+
+
+trainer = AutomaticRlTlTraining(config) 
+
+new_model = trainer.train()
+
+
