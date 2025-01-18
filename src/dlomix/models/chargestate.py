ad6fb95d01820a86c698fcc5e20d291629717cbc
omsh <omar.shouman@gmail.com>
2025-01-18 12:58:11 +0200

    cleanup - docstrings- initial tests

--- src/dlomix/models/chargestate.py
+++ src/dlomix/models/chargestate.py
@@ -1,34 +1,46 @@
 import tensorflow as tf
-import tensorflow.keras.backend as K
-from tensorflow.keras import constraints, initializers, regularizers
 
 from ..constants import ALPHABET_UNMOD
+from ..layers.attention import AttentionLayer
 
 """
---------------------------------------------------------------
-TASK MODELS
-DominantChargeStatePredictor
+This module contains models for predicting charge states for peptide sequences in mass spectrometry data.
+There are three task formulations, each with a corresponding model:
 
-ObservedChargeStatePredictor
+1. DominantChargeStatePredictor:
+   - Task: Predict the dominant charge state of a given peptide sequence.
+   - Model: Uses a deep learning model (RNN-based) inspired by Prosit's architecture to predict the most likely charge state.
 
-ChargeStateProportionPredictor
+2. ObservedChargeStatePredictor:
+   - Task: Predict the observed charge states for a given peptide sequence.
+   - Model: Uses a multi-label classification approach to predict all possible charge states.
 
---------------------------------------------------------------
+3. ChargeStateProportionPredictor:
+   - Task: Predict the proportion of each charge state for a given peptide sequence.
+   - Model: Uses a regression approach to predict the proportion of each charge state.
 """
 
 
 class DominantChargeStatePredictor(tf.keras.Model):
     """
-    Task 1
-    Predict the dominant charge state
-    Prosite Charge-State Adaption
+    Charge State Prediction Model for predicting the dominant charge state of a peptide sequence.
+
+    Args:
+        embedding_output_dim (int): The size of the embedding output dimension. Defaults to 16.
+        seq_length (int): The length of the input sequence. Defaults to 30.
+        alphabet (dict): Dictionary mapping for the alphabet (the amino acids in this case). Defaults to ALPHABET_UNMOD.
+        dropout_rate (float): The dropout rate used in the encoder layers. Defaults to 0.5.
+        latent_dropout_rate (float): The dropout rate for the latent space. Defaults to 0.1.
+        recurrent_layers_sizes (tuple): The sizes of the recurrent layers. Defaults to (256, 512).
+        regressor_layer_size (int): The size of the regressor layer. Defaults to 512.
+        num_classes (int): The number of classes for the output corresponding to charge states available in the data. Defaults to 6.
     """
 
     def __init__(
         self,
         embedding_output_dim=16,
         seq_length=30,
-        vocab_dict=ALPHABET_UNMOD,
+        alphabet=ALPHABET_UNMOD,
         dropout_rate=0.5,
         latent_dropout_rate=0.1,
         recurrent_layers_sizes=(256, 512),
@@ -38,7 +50,7 @@ class DominantChargeStatePredictor(tf.keras.Model):
         super(DominantChargeStatePredictor, self).__init__()
 
         # tie the count of embeddings to the size of the vocabulary (count of amino acids)
-        self.embeddings_count = len(vocab_dict) + 2
+        self.embeddings_count = len(alphabet)
 
         self.dropout_rate = dropout_rate
         self.latent_dropout_rate = latent_dropout_rate
@@ -90,15 +102,24 @@ class DominantChargeStatePredictor(tf.keras.Model):
 
 class ObservedChargeStatePredictor(tf.keras.Model):
     """
-    Task 2
-    Predict all observed charge states
+    Charge State Prediction Model for predicting all observed charge states of a peptide sequence.
+
+    Args:
+        embedding_output_dim (int): The size of the embedding output dimension. Defaults to 16.
+        seq_length (int): The length of the input sequence. Defaults to 30.
+        alphabet (dict): Dictionary mapping for the alphabet (the amino acids in this case). Defaults to ALPHABET_UNMOD.
+        dropout_rate (float): The dropout rate used in the encoder layers. Defaults to 0.5.
+        latent_dropout_rate (float): The dropout rate for the latent space. Defaults to 0.1.
+        recurrent_layers_sizes (tuple): The sizes of the recurrent layers. Defaults to (256, 512).
+        regressor_layer_size (int): The size of the regressor layer. Defaults to 512.
+        num_classes (int): The number of classes for the output corresponding to charge states available in the data. Defaults to 6.
     """
 
     def __init__(
         self,
         embedding_output_dim=16,
         seq_length=30,
-        vocab_dict=ALPHABET_UNMOD,
+        alphabet=ALPHABET_UNMOD,
         dropout_rate=0.5,
         latent_dropout_rate=0.1,
         recurrent_layers_sizes=(256, 512),
@@ -108,7 +129,7 @@ class ObservedChargeStatePredictor(tf.keras.Model):
         super(ObservedChargeStatePredictor, self).__init__()
 
         # tie the count of embeddings to the size of the vocabulary (count of amino acids)
-        self.embeddings_count = len(vocab_dict) + 2
+        self.embeddings_count = len(alphabet)
 
         self.dropout_rate = dropout_rate
         self.latent_dropout_rate = latent_dropout_rate
@@ -160,15 +181,24 @@ class ObservedChargeStatePredictor(tf.keras.Model):
 
 class ChargeStateDistributionPredictor(tf.keras.Model):
     """
-    Task 3
-    Predict the proportion of all observed charge states
+    Charge State Prediction Model for predicting the proportion of all observed charge states of a peptide sequence.
+
+    Args:
+        embedding_output_dim (int): The size of the embedding output dimension. Defaults to 16.
+        seq_length (int): The length of the input sequence. Defaults to 30.
+        alphabet (dict): Dictionary mapping for the alphabet (the amino acids in this case). Defaults to ALPHABET_UNMOD.
+        dropout_rate (float): The dropout rate used in the encoder layers. Defaults to 0.5.
+        latent_dropout_rate (float): The dropout rate for the latent space. Defaults to 0.1.
+        recurrent_layers_sizes (tuple): The sizes of the recurrent layers. Defaults to (256, 512).
+        regressor_layer_size (int): The size of the regressor layer. Defaults to 512.
+        num_classes (int): The number of classes for the output corresponding to charge states available in the data. Defaults to 6.
     """
 
     def __init__(
         self,
         embedding_output_dim=16,
         seq_length=30,
-        vocab_dict=ALPHABET_UNMOD,
+        alphabet=ALPHABET_UNMOD,
         dropout_rate=0.5,
         latent_dropout_rate=0.1,
         recurrent_layers_sizes=(256, 512),
@@ -178,7 +208,7 @@ class ChargeStateDistributionPredictor(tf.keras.Model):
         super(ChargeStateDistributionPredictor, self).__init__()
 
         # tie the count of embeddings to the size of the vocabulary (count of amino acids)
-        self.embeddings_count = len(vocab_dict) + 2
+        self.embeddings_count = len(alphabet)
 
         self.dropout_rate = dropout_rate
         self.latent_dropout_rate = latent_dropout_rate
@@ -226,94 +256,3 @@ class ChargeStateDistributionPredictor(tf.keras.Model):
         x = self.regressor(x)
         x = self.output_layer(x)
         return x
-
-
-class AttentionLayer(tf.keras.layers.Layer):
-    def __init__(
-        self,
-        context=False,
-        W_regularizer=None,
-        b_regularizer=None,
-        u_regularizer=None,
-        W_constraint=None,
-        b_constraint=None,
-        u_constraint=None,
-        bias=True,
-        **kwargs
-    ):
-        self.supports_masking = True
-        self.init = initializers.get("glorot_uniform")
-        self.W_regularizer = regularizers.get(W_regularizer)
-        self.b_regularizer = regularizers.get(b_regularizer)
-        self.u_regularizer = regularizers.get(u_regularizer)
-        self.W_constraint = constraints.get(W_constraint)
-        self.b_constraint = constraints.get(b_constraint)
-        self.u_constraint = constraints.get(u_constraint)
-        self.bias = bias
-        self.context = context
-        super(AttentionLayer, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        assert len(input_shape) == 3
-        self.W = self.add_weight(
-            shape=(input_shape[-1],),
-            initializer=self.init,
-            name="{}_W".format(self.name),
-            regularizer=self.W_regularizer,
-            constraint=self.W_constraint,
-        )
-        if self.bias:
-            self.b = self.add_weight(
-                shape=(input_shape[1],),
-                initializer="zero",
-                name="{}_b".format(self.name),
-                regularizer=self.b_regularizer,
-                constraint=self.b_constraint,
-            )
-        else:
-            self.b = None
-        if self.context:
-            self.u = self.add_weight(
-                shape=(input_shape[-1],),
-                initializer=self.init,
-                name="{}_u".format(self.name),
-                regularizer=self.u_regularizer,
-                constraint=self.u_constraint,
-            )
-
-        self.built = True
-
-    def compute_mask(self, input, input_mask=None):
-        return None
-
-    def call(self, x, mask=None):
-        a = K.squeeze(K.dot(x, K.expand_dims(self.W)), axis=-1)
-        if self.bias:
-            a += self.b
-        a = K.tanh(a)
-        if self.context:
-            a = K.squeeze(K.dot(x, K.expand_dims(self.u)), axis=-1)
-        a = K.exp(a)
-        if mask is not None:
-            a *= K.cast(mask, K.floatx())
-        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
-        a = K.expand_dims(a)
-        weighted_input = x * a
-        return K.sum(weighted_input, axis=1)
-
-    def compute_output_shape(self, input_shape):
-        return input_shape[0], input_shape[-1]
-
-    def get_config(self):
-        config = {
-            "bias": self.bias,
-            "context": self.context,
-            "W_regularizer": regularizers.serialize(self.W_regularizer),
-            "b_regularizer": regularizers.serialize(self.b_regularizer),
-            "u_regularizer": regularizers.serialize(self.u_regularizer),
-            "W_constraint": constraints.serialize(self.W_constraint),
-            "b_constraint": constraints.serialize(self.b_constraint),
-            "u_constraint": constraints.serialize(self.u_constraint),
-        }
-        base_config = super(AttentionLayer, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
