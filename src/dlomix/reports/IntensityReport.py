8b70e69d824e408ecd3f1d8b47a1d98fdc6a3ec2
Omar Shouman <o.shouman@tum.de>
2025-01-20 20:18:38 +0200

    Feature/update reports (#51)
    * Intensity report updates
    
    * example intensity notebook

--- src/dlomix/reports/IntensityReport.py
+++ src/dlomix/reports/IntensityReport.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 from matplotlib.ticker import LogLocator
 
+from ..data.processing.processors import SequenceParsingProcessor
 from .postprocessing import normalize_intensity_predictions
 from .Report import PDFFile, Report
 
@@ -17,6 +18,7 @@ class IntensityReport(Report):
     TARGETS_LABEL = "x"
     PREDICTIONS_LABEL = "y"
     DEFAULT_BATCH_SIZE = 600
+    PREDICTIONS_COL_NAME = "intensities_pred"
 
     def __init__(self, output_path, history, figures_ext="png", batch_size=0):
         super(IntensityReport, self).__init__(output_path, history, figures_ext)
@@ -28,39 +30,63 @@ class IntensityReport(Report):
         else:
             self.batch_size = IntensityReport.DEFAULT_BATCH_SIZE
 
-    def generate_report(self, dataset, predictions):
+    def generate_report(
+        self,
+        dataset,
+        predictions,
+        split="test",
+        precursor_charge_column_name="precursor_charge_onehot",
+    ):
         self._init_report_resources()
 
-        predictions_df = self.generate_intensity_results_df(dataset, predictions)
         self.plot_all_metrics()
 
         # make custom plots
-        self.plot_spectral_angle(predictions_df)
+        self.plot_spectral_angle(
+            dataset, predictions, split, precursor_charge_column_name
+        )
 
         self._compile_report_resources_add_pdf_pages()
         self.pdf_file.output(join(self._output_path, "intensity_Report.pdf"), "F")
 
-    def generate_intensity_results_df(self, dataset, predictions):
-        predictions_df = pd.DataFrame()
-
-        predictions_df["sequences"] = dataset.sequences
-        predictions_df["intensities_pred"] = predictions.tolist()
-        predictions_df["precursor_charge_onehot"] = dataset.precursor_charge.tolist()
-        predictions_df["intensities_raw"] = dataset.intensities.tolist()
-
-        return predictions_df
-
-    def plot_spectral_angle(self, predictions_df):
+    def plot_spectral_angle(
+        self, dataset, predictions, split, precursor_charge_column_name
+    ):
         """Create spectral  plot
 
         Arguments
         ---------
-            predictions_df:  dataframe with raw intensities, predictions, sequences, precursor_charges
+        dataset:  FragmentIonIntensityDataset
+        predictions:  array of predictions
+        split:  str for the split name in the FragmentIonIntensityDataset
+        precursor_charge_column_name:  str
         """
 
-        predictions_acc = normalize_intensity_predictions(
-            predictions_df, self.batch_size
+        predictions_df = (
+            dataset[split]
+            .select_columns(
+                [
+                    SequenceParsingProcessor.PARSED_COL_NAMES["seq"],
+                    dataset.label_column,
+                    *dataset.model_features,
+                ]
+            )
+            .to_pandas()
         )
+
+        predictions_df[IntensityReport.PREDICTIONS_COL_NAME] = predictions.tolist()
+
+        predictions_acc = normalize_intensity_predictions(
+            predictions_df,
+            sequence_column_name=SequenceParsingProcessor.PARSED_COL_NAMES["seq"],
+            labels_column_name=dataset.label_column,
+            predictions_column_name=IntensityReport.PREDICTIONS_COL_NAME,
+            precursor_charge_column_name=precursor_charge_column_name,
+            batch_size=self.batch_size,
+        )
+
+        self.prediction_results = predictions_acc
+
         violin_plot = sns.violinplot(predictions_acc["spectral_angle"])
 
         save_path = join(
