10b007d1e888a3e33fe23fec47d6aece85d86cd2
Omar Shouman <o.shouman@tum.de>
2025-01-29 22:18:05 +0200

    Feature/add precursor charge prediction modules (#55)
    * chargestate models and init updates
    
    * dev notebooks for chargestate
    
    * cleanup - docstrings- initial tests
    
    * run script for dominant charge
    
    * fix: added hf_hub dependency by default
    
    * Adding Noahs changes & trained model to Florians / Omars CSD prediction repo (#54)
    
    
    ---------
    
    Co-authored-by: omsh <omar.shouman@gmail.com>
    
    ---------
    
    Co-authored-by: radox96 <boessl.florian@t-online.de>
    Co-authored-by: Ayla <131137738+ayla-s@users.noreply.github.com>

--- src/dlomix/eval/__init__.py
+++ src/dlomix/eval/__init__.py
@@ -1,3 +1,4 @@
+from .chargestate import adjusted_mean_absolute_error
 from .rt_eval import TimeDeltaMetric, TimeDeltaMetric2
 
-__all__ = ["TimeDeltaMetric", "TimeDeltaMetric2"]
+__all__ = ["TimeDeltaMetric", "TimeDeltaMetric2", "adjusted_mean_absolute_error"]
