a600077fb6f15eeabbc2e0f8147842934b6733da
omsh <omar.shouman@gmail.com>
2025-04-14 19:13:27 +0200

    refactoring in import messages

--- src/dlomix/__init__.py
+++ src/dlomix/__init__.py
@@ -1,23 +1,22 @@
-from .config import _BACKEND, PYTORCH_BACKEND, TENSORFLOW_BACKEND
+from .config import _BACKEND, BACKEND_PRETTY_NAME, PYTORCH_BACKEND, TENSORFLOW_BACKEND
+
+BACKEND_IMPORT_ERROR_MESSAGE = f"""
+{BACKEND_PRETTY_NAME} is selected as backend, but it is not installed.
+Please install DLOmix with {BACKEND_PRETTY_NAME} using 'pip install dlomix[{BACKEND_PRETTY_NAME.lower()}]'
+or separately install the required backend via pip.
+"""
+
 
 if _BACKEND in PYTORCH_BACKEND:
     try:
         import torch  # Check for PyTorch
     except ImportError:
-        raise ImportError(
-            "PyTorch is selected as backend, but it is not installed. Please install DLOmix with PyTorch"
-            "using  'pip install dlomix[torch]'"
-            "or separately install the required backend."
-        )
+        raise ImportError(BACKEND_IMPORT_ERROR_MESSAGE)
 if _BACKEND in TENSORFLOW_BACKEND:
     try:
         import tensorflow  # Check for TensorFlow
     except ImportError:
-        raise ImportError(
-            "TensorFlow is the default backend or was selected, but it is not installed. Please install DLOmix with TensorFlow"
-            "using  'pip install dlomix[tensorflow]'"
-            "or separately install the required backend."
-        )
+        raise ImportError(BACKEND_IMPORT_ERROR_MESSAGE)
 
 __all__ = [
     "_BACKEND",  # Also expose the current backend name
--- src/dlomix/config.py
+++ src/dlomix/config.py
@@ -2,32 +2,36 @@ import os
 import warnings
 
 DEFAULT_BACKEND = "tensorflow"
+BACKEND_PRETTY_NAME = "TensorFlow"
+
 TENSORFLOW_BACKEND = ["tensorflow", "tf"]
 PYTORCH_BACKEND = ["pytorch", "torch", "pt"]
 
 
+def custom_show_warning(message, category, filename, lineno, file=None, line=None):
+    print(f"{message}")
+
+
 # Allow setting backend via environment variable or default to tensorflow
-_BACKEND = os.environ.get("DLOMIX_BACKEND", DEFAULT_BACKEND).lower()
+_BACKEND = os.environ.get("DLOMIX_BACKEND", DEFAULT_BACKEND).lower().strip()
 
 if _BACKEND in TENSORFLOW_BACKEND:
     BACKEND_PRETTY_NAME = "TensorFlow"
 elif _BACKEND in PYTORCH_BACKEND:
     BACKEND_PRETTY_NAME = "PyTorch"
 else:
-    raise ValueError(
-        f"DLOmix supports 'tensorflow' or 'pytorch' backends, got '{_BACKEND}'"
-    )
+    # default to TensorFlow
+    with warnings.catch_warnings():
+        warnings.simplefilter("once", category=UserWarning)  # Show only once
+        warnings.showwarning = (
+            custom_show_warning  # Temporarily override the default showwarning
+        )
+        warnings.warn(
+            f"Backend '{_BACKEND}' is not supported. Defaulting to {BACKEND_PRETTY_NAME} backend.",
+            UserWarning,
+        )
 
-message = f"""
-
-Using {BACKEND_PRETTY_NAME} Backend for DLOmix.
-To change the backend, set the DLOMIX_BACKEND environment variable to tensorflow or pytorch and re-import DLOmix."
-
-"""
-
-
-def custom_show_warning(message, category, filename, lineno, file=None, line=None):
-    print(f"{message}")
+    _BACKEND = DEFAULT_BACKEND
 
 
 with warnings.catch_warnings():
@@ -35,4 +39,6 @@ with warnings.catch_warnings():
     warnings.showwarning = (
         custom_show_warning  # Temporarily override the default showwarning
     )
+
+    message = f"Using {BACKEND_PRETTY_NAME} Backend for DLOmix. To change the backend, set the DLOMIX_BACKEND environment variable to tensorflow or pytorch and re-import DLOmix."
     warnings.warn(message, UserWarning)  # Trigger the warning
