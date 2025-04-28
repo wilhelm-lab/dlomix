422849613b31067d183070d3baa2832b84c2014d
omsh <omar.shouman@gmail.com>
2025-04-28 14:52:44 +0300

    intensity changes + ptms

--- run_scripts/run_prosit_intensity_ptms_torch.py
+++ run_scripts/run_prosit_intensity_ptms_torch.py
@@ -1,6 +1,6 @@
 """
     To run this script, use the following command:
-    DLOMIX_BACKEND=torch python run_scripts/run_prosit_intensity_torch.py
+    DLOMIX_BACKEND=torch python run_scripts/run_prosit_intensity_ptms_torch.py
 """
 
 import logging
@@ -18,20 +18,20 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
-BATCH_SIZE = 128
+BATCH_SIZE = 8
+N_EPOCHS = 20
 
 model = PrositIntensityPredictor(
-    seq_length=30,
-    use_prosit_ptm_features=False,
+    seq_length=32,
+    use_prosit_ptm_features=True,
     input_keys={
         "SEQUENCE_KEY": "modified_sequence",
     },
-    # meta_data_keys={
-    #     "COLLISION_ENERGY_KEY": "collision_energy_aligned_normed",
-    #     "PRECURSOR_CHARGE_KEY": "precursor_charge_onehot",
-    # },
-    with_termini=False,
-    # alphabet=ALPHABET_UNMOD,
+    meta_data_keys={
+        "COLLISION_ENERGY_KEY": "collision_energy_aligned_normed",
+        "PRECURSOR_CHARGE_KEY": "precursor_charge_onehot",
+    },
+    with_termini=True,
 )
 
 optimizer = torch.optim.Adam(params=model.parameters(), lr=0.0001)
@@ -40,23 +40,21 @@ TRAIN_DATAPATH = "example_dataset/intensity/third_pool_processed_sample.parquet"
 
 d = FragmentIonIntensityDataset(
     data_source=TRAIN_DATAPATH,
-    max_seq_len=30,
+    max_seq_len=32,
     batch_size=BATCH_SIZE,
     val_ratio=0.2,
     model_features=["collision_energy_aligned_normed", "precursor_charge_onehot"],
     sequence_column="modified_sequence",
     label_column="intensities_raw",
-    # features_to_extract=["mod_loss", "delta_mass"],
+    features_to_extract=["mod_loss", "delta_mass"],
     dataset_type="pt",
-    # alphabet=ALPHABET_UNMOD,
-    with_termini=False,
+    with_termini=True,
 )
 
 print(d)
 
 loss_criterion = masked_spectral_distance
 
-N_EPOCHS = 20
 for epoch in tqdm(range(0, N_EPOCHS)):
     epoch_loss = 0
     model.train()
@@ -93,3 +91,6 @@ for epoch in tqdm(range(0, N_EPOCHS)):
 
         avg_val_loss = val_loss_total / val_data_size
     print(f"Epoch {epoch} Summary:  Validation Loss: {avg_val_loss:.4f}")
+
+print(val_pred_cs.shape)
+print(val_pred_cs[0])
--- src/dlomix/models/prosit_torch.py
+++ src/dlomix/models/prosit_torch.py
@@ -1,12 +1,17 @@
+import logging
+import warnings
 from collections import OrderedDict
 
 import torch
 import torch.nn as nn
 
 from ..constants import ALPHABET_UNMOD
+from ..data.processing.feature_extractors import FEATURE_EXTRACTORS_PARAMETERS
 from ..layers.attention_torch import AttentionLayer
 from ..layers.bi_gru_seq_encoder_torch import BiGRUSequentialEncoder
 from ..layers.gru_seq_decoder_torch import GRUSequentialDecoder
+
+logger = logging.getLogger("dlomix.models.prosit_torch")
 
 
 class PrositRetentionTimePredictor(nn.Module):
@@ -99,6 +104,35 @@ class PrositRetentionTimePredictor(nn.Module):
 
 
 class PrositIntensityPredictor(nn.Module):
+    """
+    Implementation of the Prosit model for fragment ion intensity prediction.
+
+    Parameters
+    -----------
+
+
+
+
+
+    """
+
+    DEFAULT_INPUT_KEYS = {
+        "SEQUENCE_KEY": "sequence",
+        "COLLISION_ENERGY_KEY": "collision_energy",
+        "PRECURSOR_CHARGE_KEY": "precursor_charge",
+        "FRAGMENTATION_TYPE_KEY": "fragmentation_type",
+    }
+
+    # can be extended to include all possible meta data
+    META_DATA_KEYS = [
+        "COLLISION_ENERGY_KEY",
+        "PRECURSOR_CHARGE_KEY",
+        "FRAGMENTATION_TYPE_KEY",
+    ]
+
+    # retrieve the Lookup PTM feature keys
+    PTM_INPUT_KEYS = [*FEATURE_EXTRACTORS_PARAMETERS.keys()]
+
     def __init__(
         self,
         embedding_output_dim=16,
@@ -140,7 +174,7 @@ class PrositIntensityPredictor(nn.Module):
             self.alphabet = ALPHABET_UNMOD
 
         # tie the count of embeddings to the size of the vocabulary (count of amino acids)
-        self.embeddings_count = len(self.alphabet)
+        self.embeddings_count = len(self.alphabet) + 1
 
         self.embedding = nn.Embedding(
             num_embeddings=self.embeddings_count,
@@ -155,14 +189,8 @@ class PrositIntensityPredictor(nn.Module):
         )
 
         self.meta_data_fusion_layer = None
-        # ToDo: test with metadata keys
-        # if self.meta_data_keys: ---> Still needs to be fixed
-        #     self.meta_data_fusion_layer = nn.Sequential(
-        #         OrderedDict([
-        #             torch.mul(name="add_meta"),
-        #             torch.Tensor.repeat(self.max_ion, name="repeat"),
-        #         ])
-        #     )
+        if self.meta_data_keys:
+            self.meta_data_fusion_layer = MetaDataFusionBlock(max_ion=self.max_ion)
 
         self.regressor = nn.Sequential(
             OrderedDict(
@@ -176,18 +204,23 @@ class PrositIntensityPredictor(nn.Module):
 
     def _build_encoders(self):
         # sequence encoder -> always present
+        gru_features_input_size = self.embedding_output_dim
+
+        if self.use_prosit_ptm_features:
+            gru_features_input_size = self.embedding_output_dim * 2
+
         self.sequence_encoder = BiGRUSequentialEncoder(
-            embedding_output_dim=self.embedding_output_dim,
+            embedding_output_dim=gru_features_input_size,
             recurrent_layers_sizes=self.recurrent_layers_sizes,
             dropout_rate=self.dropout_rate,
         )
 
-        # meta data encoder -> optional, only if meta data keys are provided -- not yet adapted
+        # meta data encoder -> optional, only if meta data keys are provided
         self.meta_encoder = None
         if self.meta_data_keys:
             self.meta_encoder = nn.Sequential(
                 OrderedDict(
-                    [  # use cat? or is nn.seq fine?
+                    [
                         (
                             "meta_dense",
                             nn.LazyLinear(out_features=self.recurrent_layers_sizes[1]),
@@ -197,32 +230,20 @@ class PrositIntensityPredictor(nn.Module):
                 )
             )
 
-        # ptm encoder -> optional, only if ptm flag is provided -- not yet properly adapted
+        # ptm encoder -> optional, only if ptm flag is provided
         self.ptm_input_encoder, self.ptm_aa_fusion = None, None
         if self.use_prosit_ptm_features:
             self.ptm_input_encoder = nn.Sequential(
-                OrderedDict(
-                    [
-                        (
-                            "linear1",
-                            nn.LazyLinear(out_features=self.regressor_layer_size // 2),
-                        ),
-                        ("dropout1", nn.Dropout(p=self.dropout_rate)),
-                        (
-                            "linear2",
-                            nn.LazyLinear(out_features=self.embedding_output_dim * 4),
-                        ),
-                        ("dropout2", nn.Dropout(p=self.dropout_rate)),
-                        (
-                            "linear3",
-                            nn.LazyLinear(out_features=self.embedding_output_dim),
-                        ),
-                        ("dropout3", nn.Dropout(p=self.dropout_rate)),
-                    ]
-                )
+                Concatenate(dim=-1),
+                nn.LazyLinear(out_features=self.regressor_layer_size // 2),
+                nn.Dropout(p=self.dropout_rate),
+                nn.LazyLinear(out_features=self.embedding_output_dim * 4),
+                nn.Dropout(p=self.dropout_rate),
+                nn.LazyLinear(out_features=self.embedding_output_dim),
+                nn.Dropout(p=self.dropout_rate),
             )
 
-        # self.ptm_aa_fusion = torch.cat((self.sequence_encoder, self.ptm_input_encoder))
+            self.ptm_aa_fusion = Concatenate(dim=-1)
 
     def _build_decoder(self):
         self.decoder = GRUSequentialDecoder(
@@ -261,22 +282,28 @@ class PrositIntensityPredictor(nn.Module):
 
             # ToDo: ensure PTM features work as expected
             # read PTM features from the input dict # --> Still needs to be implemented
-            # ptm_ac_features = self._collect_values_from_inputs_if_exists(
-            #     inputs, PrositIntensityPredictor.PTM_INPUT_KEYS
-            # )
+            ptm_ac_features = self._collect_values_from_inputs_if_exists(
+                inputs, PrositIntensityPredictor.PTM_INPUT_KEYS
+            )
 
-            # if self.ptm_input_encoder and len(ptm_ac_features) > 0:
-            #     encoded_ptm = self.ptm_input_encoder(ptm_ac_features)
-            # elif self.use_prosit_ptm_features:
-            #     warnings.warn(
-            #         f"PTM features enabled and following PTM features are expected in the model for Prosit Intesity: {PrositIntensityPredictor.PTM_INPUT_KEYS}. The actual input passed to the model contains the following keys: {list(inputs.keys())}. Falling back to no PTM features."
-            #     )
+            if self.ptm_input_encoder and len(ptm_ac_features) > 0:
+                logger.debug("PTM features: ")
+                for f in ptm_ac_features:
+                    logger.debug(f.shape)
+                encoded_ptm = self.ptm_input_encoder(ptm_ac_features)
+            elif self.use_prosit_ptm_features:
+                warnings.warn(
+                    f"PTM features enabled and following PTM features are expected in the model for Prosit Intesity: {PrositIntensityPredictor.PTM_INPUT_KEYS}. The actual input passed to the model contains the following keys: {list(inputs.keys())}. Falling back to no PTM features."
+                )
 
         x = self.embedding(peptides_in)
 
         # fusion of PTMs (before going into the GRU sequence encoder)
         if self.ptm_aa_fusion and encoded_ptm is not None:
+            logger.debug("before ptm fusion: %s", x.shape)
+            logger.debug("before ptm fusion enc ptm: %s", encoded_ptm.shape)
             x = self.ptm_aa_fusion([x, encoded_ptm])
+            logger.debug("concatednated after fusion: %s", x.shape)
 
         x = self.sequence_encoder(x)
 
@@ -312,3 +339,29 @@ class PrositIntensityPredictor(nn.Module):
                     single_input = torch.unsqueeze(single_input, axis=-1)
                 collected_values.append(single_input)
         return collected_values
+
+
+class Concatenate(nn.Module):
+    def __init__(self, dim=-1):
+        super(Concatenate, self).__init__()
+        self.dim = dim
+
+    def forward(self, inputs):
+        for x in inputs:
+            logger.debug("concatenating: %s ", x.shape)
+        return torch.cat(inputs, dim=self.dim)
+
+
+class MetaDataFusionBlock(torch.nn.Module):
+    def __init__(self, max_ion):
+        super(MetaDataFusionBlock, self).__init__()
+        self.max_ion = max_ion
+
+    def forward(self, x):
+        #  x is a tuple of (features, metadata)
+        features, metadata = x
+        # Multiply operation
+        multiplied = features * metadata
+        # RepeatVector equivalent - expand along dimension to repeat max_ion times
+        repeated = multiplied.unsqueeze(1).expand(-1, self.max_ion, -1)
+        return repeated
