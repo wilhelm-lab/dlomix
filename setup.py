cae750fe586e0928944a4b08676f01e091d5568c
omsh <omar.shouman@gmail.com>
2026-03-30 11:41:58 +0200

    dropped python 3.9 from the pipeline

--- setup.py
+++ setup.py
@@ -72,10 +72,8 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
