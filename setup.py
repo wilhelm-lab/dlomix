3cff53ba11a5ca2081ce7c20c36ca592605ed224
Omar Shouman <o.shouman@tum.de>
2026-08-14 11:09:43 +0200

    json files instead of pickles (#99)
    * json files instead of pickles
    
    * formatting for readability
    
    * combined gain+loss json file - removed +1 from original features
    
    * remove -1 from gain and move to loss
    
    ---------
    
    Co-authored-by: WassimG <wassim.gabriel@gmail.com>

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    package_data={"": ["data/processing/pickled_feature_dicts/*"]},
+    package_data={"": ["data/processing/feature_dicts/*"]},
     python_requires=">=3.10",
     install_requires=[
         "datasets>=4.0.0",
--- src/dlomix/data/inference.py
+++ src/dlomix/data/inference.py
@@ -116,6 +116,7 @@ class PeptidePreprocessor:
             encoding_extend_alphabet=False,
             encoding_fallback_unmodified=True,
             warn_unmod=False,
+            fail_on_missing_modified_amino_acid=False,
         )
 
     @property
